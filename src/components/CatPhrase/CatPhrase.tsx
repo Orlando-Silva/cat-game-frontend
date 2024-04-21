c1f2554260cacdb0e6f564e72436cc5e8bf87fa2
samuel-marafigo <125743714+samuel-marafigo@users.noreply.github.com>
2024-04-21 14:16:28 -0300

    Add tests to CatPhrase component

--- src/components/CatPhrase/CatPhrase.tsx
+++ src/components/CatPhrase/CatPhrase.tsx
@@ -11,6 +11,7 @@ const CatPhrase: React.FunctionComponent<CatPhraseProps> = ({ text, onSelectPhra
       <button 
         type="button" 
         className='font-sans px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm opacity-100 hover:-translate-y-1 hover:scale-110 p-2 duration-300' 
+        data-testid={"cat-phrase"}
         onClick={() => onSelectPhrase()} >
           { text }
       </button>      
